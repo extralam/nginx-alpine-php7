160f3bd0d7203ebab873aa6fcbda0a7a0b7e32b5
alan <certain603@gmail.com>
2017-10-09 18:44:14 +0800

    update Dockerfile path
    update Dockerfile path

--- docker/script/build.sh
+++ docker/script/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -f ./docker/Dockerfile -t extralam/nginx-alpine-php7:$1 .
+docker build -f ./Dockerfile -t extralam/nginx-alpine-php7:$1 .
 docker tag extralam/nginx-alpine-php7:$1 extralam/nginx-alpine-php7:latest
 docker push extralam/nginx-alpine-php7:$1
 docker push extralam/nginx-alpine-php7:latest
